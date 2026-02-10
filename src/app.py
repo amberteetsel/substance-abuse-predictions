2378f589e2ff6efd44250371eb196c64865ba658
Andrea Caceres <120444268+organiccaprisunn@users.noreply.github.com>
2026-02-09 17:29:49 -0700

    added my latest edits to main

--- src/app.py
+++ src/app.py
@@ -77,8 +77,8 @@ with tab3:
     with c1:
         st.image("bio/AndreaCaceres_Headshot.jpg", width=150)
         st.write("**Andrea Caceres**")
-        st.write("Role: Modeling and Visualization Lead")
-        st.write("Andrea Caceres is a graduate student at the University of Colorado Boulder with a Bachelors in Statistics from the University of Georgia. She has experience in pensions analysis from her prior position at WTW, but is focusing on expanding her data analytical knowledge. ")
+        st.write("Role: Modeling & Visualization Lead")
+        st.write("Andrea Caceres is a graduate student at the University of Colorado Boulder with a Bachelors in Statistics from the University of Georgia. She has experience in pension analysis from her prior position at WTW, but is focusing on expanding her data analytical knowledge. ")
         st.caption("[GitHub](https://github.com/andreaycaceres)")
         st.caption("[LinkedIn](https://www.linkedin.com/in/andrea-caceres-609609256/)")
 
@@ -86,7 +86,7 @@ with tab3:
         st.image("bio/IsraMarcu_Headshot.JPG", width=150)
         st.write("**Isra Marcu**")
         st.write("Role: Data & Analyzation Lead ")
-        st.write("Isra Marcu is a graduate student at the University of Colorado Boulder pursuing a Master’s in Data Science. She has a Bachelors in Psychology from the University of North Carolina at Chapel Hill. Isra has experience in ethically conducting research on participants and analyzing corresponding data.")
+        st.write("Isra Marcu is a graduate student at the University of Colorado Boulder pursuing a Masters in Data Science. She has a Bachelors in Psychology from the University of North Carolina at Chapel Hill. Isra has experience in ethically conducting research on participants and analyzing corresponding data.")
         st.caption("[GitHub](https://github.com/isram1223)")
         st.caption("[LinkedIn](https://www.linkedin.com/in/isra-marcu-a220a1274/)")
 
@@ -94,7 +94,7 @@ with tab3:
         st.image("bio/Teetsel_Headshot.jpeg", width=150)
         st.write("**Amber Teetsel**")
         st.write("Role: Web Developer & Data Scientist")
-        st.write("Bio: Amber Teetsel is currently pursuiing a Master's in Data Science at the University of Colorado Boulder. She has undergraduate degrees in Mathematics, Finance, and Women's Studies. Amber began her career in financial services consulting, but her past 4 years of experience have been as data analyst for a multinational corporation.")
+        st.write("Bio: Amber Teetsel is currently pursuing a Master's in Data Science at the University of Colorado Boulder. She has undergraduate degrees in Mathematics, Finance, and Women's Studies. Amber began her career in financial services consulting, but her past 4 years of experience have been as data analyst for a multinational corporation.")
         st.caption("[LinkedIn](https://www.linkedin.com/in/amber-teetsel/)")
         st.caption("[GitHub](https://github.com/amberteetsel)")
 
